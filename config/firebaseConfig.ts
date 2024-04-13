25fb8827536507f7c3426539f264dd5943249316
rauno-tali <93924339+rauno-tali@users.noreply.github.com>
2024-04-13 21:25:01 +0300

    add auth, functions and firestore emulators

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,4 +1,8 @@
 import { initializeApp } from "firebase/app";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
@@ -13,3 +17,12 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore();
+const functions = getFunctions(app);
+const auth = getAuth();
+
+connectAuthEmulator(auth, "http://localhost:9099");
+connectFunctionsEmulator(functions, "localhost", 5001);
+connectFirestoreEmulator(db, "localhost", 8080);
+
+export { auth, functions, app, db };
