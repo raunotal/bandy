c38a97e17e017e482977b55e8161d5d16ec785c1
rauno-tali <93924339+rauno-tali@users.noreply.github.com>
2024-04-20 22:19:08 +0300

    INTERNAL - remove firebase analytics from project

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -3,8 +3,6 @@ import { getAuth, connectAuthEmulator } from "firebase/auth";
 import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
-// import { getAnalytics } from "firebase/analytics";
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -16,7 +14,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 const firestore = getFirestore();
 const functions = getFunctions(app);
 const auth = getAuth();
