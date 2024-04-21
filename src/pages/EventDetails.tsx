6cd414ddcb257b6679b7493e39e1574b1e6439d8
Rauno Tali <rauno@pats.ee>
2024-04-21 17:45:35 +0300

    refactor event details page

--- src/pages/EventDetails.tsx
+++ src/pages/EventDetails.tsx
@@ -13,18 +13,12 @@ interface EventDetailsProps
 const EventDetails: FC<EventDetailsProps> = ({ match }) => {
   const { user } = useAuth();
   const [event, setEvent] = useState<Event | null>(null);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setEvent(
       user?.events?.find((event) => event.uid === match.params.uid) ?? null
     );
-    setLoading(false);
   }, [match.params.uid]);
-
-  if (loading) {
-    return <GeneralLayout title='Events'>Loading...</GeneralLayout>;
-  }
 
   if (!event) {
     return <GeneralLayout title='Events'>Event not found</GeneralLayout>;
