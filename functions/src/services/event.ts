59629cd42afd3a88fe935ef0559602b1148f1c50
rauno-tali <93924339+rauno-tali@users.noreply.github.com>
2024-05-03 17:26:52 +0300

    INTERNAL - add import FieldValue type

--- functions/src/services/event.ts
+++ functions/src/services/event.ts
@@ -6,6 +6,7 @@ import { logger } from 'firebase-functions';
 import { Status } from '../../../enums/event';
 import { Event } from '../../../types/event';
 import { Collection } from '../../../enums/collection';
+import { FieldValue } from 'firebase-admin/firestore';
 
 const firestore = admin.firestore();
 
@@ -37,12 +38,12 @@ export const addEvent = functions.https.onCall(
         members.forEach((member) => {
           const userRef = firestore.collection('users').doc(member.uid!);
           transaction.update(userRef, {
-            events: admin.firestore.FieldValue.arrayUnion(eventRef.id),
+            events: FieldValue.arrayUnion(eventRef.id),
           });
         });
 
         transaction.update(managerRef, {
-          events: admin.firestore.FieldValue.arrayUnion(eventRef.id),
+          events: FieldValue.arrayUnion(eventRef.id),
         });
       });
 
