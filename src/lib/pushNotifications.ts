563d8a2e8f0b315537f0a160206de92163781a1a
Rauno Tali <rauno@pats.ee>
2024-05-04 14:10:40 +0300

    move VAPID key to push notification class field

--- src/lib/pushNotifications.ts
+++ src/lib/pushNotifications.ts
@@ -4,7 +4,10 @@ import { httpsCallable } from 'firebase/functions';
 import { Callable } from '../../enums/callable';
 
 class PushNotifications {
-  private static notificationsAllowed: boolean = Notification.permission === 'granted';
+  private static notificationsAllowed: boolean =
+    Notification.permission === 'granted';
+  private static VAPID_KEY: string =
+    'BGHsOtPctwGLd7-623D3pVAGUNvjI2J8clZqQyQO4kvn4iDZBbP9luV-e53V6aWtrYB1Vi79JY1xjUKgDwHPViE';
 
   public static async initPushNotifications(uid: string) {
     if (!this.notificationsAllowed) {
@@ -20,13 +23,13 @@ class PushNotifications {
     if (!this.notificationsAllowed) {
       await Notification.requestPermission();
       this.notificationsAllowed = true;
-      await this.initPushNotifications(uid)
+      await this.initPushNotifications(uid);
     }
   }
 
   private static async getFcmToken() {
     return await getToken(messaging, {
-      vapidKey: import.meta.env.VITE_FIREBASE_VAPID_KEY
+      vapidKey: this.VAPID_KEY,
     });
   }
 
@@ -35,13 +38,16 @@ class PushNotifications {
       if (payload) {
         new Notification(payload.notification!.title!, {
           body: payload.notification!.body,
-          icon: payload.notification!.icon
+          icon: payload.notification!.icon,
         });
       }
     });
   }
 
-  private static async addFcmTokenToUser(uid: string, token: string): Promise<void> {
+  private static async addFcmTokenToUser(
+    uid: string,
+    token: string
+  ): Promise<void> {
     const addFCMToken = httpsCallable(functions, Callable.AddFcmToken);
     await addFCMToken({ uid, token });
   }
